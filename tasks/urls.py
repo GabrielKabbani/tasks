8fa019be054efebac20cd049b922e6ce932016bc
Gabriel Kabbani <gabrielmk@al.insper.edu.br>
2021-12-03 18:46:07 -0300

    consertando import

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 from django.urls import include, path
-
+from django.conf.urls import url 
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
