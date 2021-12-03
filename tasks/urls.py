b8d60fc1f65c2059eee84eb8af972ea871007ef0
Gabriel Kabbani <gabrielmk@al.insper.edu.br>
2021-12-03 18:13:44 -0300

    progresso

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,6 +5,6 @@ from django.urls import include, path
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
-urlpatterns = [url(r'^api/tasks$', views.execute)]
+urlpatterns = [url(r'^request/tasks$', views.execute)]
 
 
